45e97971ca5c69e0c6046edd6b4188a32b00ade0
carlosfabianchaves <cchaves@isthmusit.com>
2020-07-05 00:10:58 -0600

    https and http test

--- src/common/Urls.js
+++ src/common/Urls.js
@@ -1,4 +1,4 @@
 export function getBaseApiUrl (){
     //return "http://orderonline.local/api";
-    return "https://ec2-3-134-211-255.us-east-2.compute.amazonaws.com/api";
+    return "//orderonlinecr.com/api";
 }
