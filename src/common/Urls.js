b8258aac16e1525e5eb20da61c47028abc31c126
carlosfabianchaves <cchaves@isthmusit.com>
2020-07-04 19:00:27 -0600

    providng aws https link

--- src/common/Urls.js
+++ src/common/Urls.js
@@ -1,4 +1,4 @@
 export function getBaseApiUrl (){
     //return "http://orderonline.local/api";
-    return "http://ec2-3-134-211-255.us-east-2.compute.amazonaws.com/api";
+    return "https://ec2-3-134-211-255.us-east-2.compute.amazonaws.com/api";
 }
