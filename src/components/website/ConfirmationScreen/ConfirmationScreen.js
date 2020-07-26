ebfe1dc7525879b4589216a49638222ff264c773
carlosfabianchaves <cchaves@isthmusit.com>
2020-07-25 22:39:29 -0600

    testing confirmation screen

--- src/components/website/ConfirmationScreen/ConfirmationScreen.js
+++ src/components/website/ConfirmationScreen/ConfirmationScreen.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react'
+import './ConfirmationScreen.scss';
 
 export default class ConfirmationScreen extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-                 
+                 locationIsActive:false
         }
     }
-
+    toggleLocation = ()=>{
+        this.setState({locationIsActive: !this.state.locationIsActive});
+    }
     render() {
         var mapSytle = {
-            width:"500px",
+            width:"80%",
             height: "500px",
         }
         return (
-            <div>
+            <div className="page-padding">
                 <h1>Confirmacion de Pedido</h1>
-                <div id="googleMap" style={mapSytle}></div>               
+                <div className="info-user">
+                    <div className="form-control">
+                        <label>Nombre:</label>
+                        <input src="" className="beauty-input"></input>
+                    </div>
+                    <div className="form-control">
+                        <label>Correo:</label>
+                        <input src="" className="beauty-input"></input>
+                    </div>
+                    <div className="form-control">
+                        <label>Telefono:</label>
+                        <input src="" className="beauty-input"></input>
+                    </div>
+                    <div className="form-control">
+                        <label>Ubicacion:</label>
+                        <input src="" placeholder="Escoge la ubicacion de entrega"
+                            className="beauty-input" disabled></input>
+                        {/* <a className="location-btn" onClick={this.toggleLocation}>
+                            {this.state.locationIsActive ? "Guardar" : "Seleccionar"}
+                        </a> */}
+                    </div>
+                </div>
+                <div id="googleMap" style={mapSytle}
+                 className={this.state.locationIsActive ? "map-active" : "map-hidden"}></div>
             </div>
         )
     }
